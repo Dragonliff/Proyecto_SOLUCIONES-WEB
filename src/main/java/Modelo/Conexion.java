09b6d1c1230a99ba384886d2a9f80228ba920b66
Dragonliff <kias29558@gmail.com>
2025-10-23 12:38:48 -0500

    JAVA

---
+++ src/main/java/Modelo/Conexion.java
@@ -0,0 +1,26 @@
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private static final String URL = "jdbc:mysql://localhost:3306/mantenimiento_predictivo2";
+    private static final String USER = "root"; // cambia si tu usuario es distinto
+    private static final String PASSWORD = "admin123"; // pon tu contraseña si tienes
+
+    private static Connection conexion;
+
+    public static Connection getConexion() {
+        try {
+            if (conexion == null || conexion.isClosed()) {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Conexión establecida con la base de datos.");
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error de conexión: " + e.getMessage());
+        }
+        return conexion;
+    }
+}
